d22579655ebb1ecef3431be23e6eb0d13e6939b5
Istvan Bartuszek <istvan.bartuszek@gmail.com>
2023-02-27 22:54:54 +0100

    add kotlin logging and detekt plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,24 +6,41 @@ plugins {
 	kotlin("jvm") version "1.7.22"
 	kotlin("plugin.spring") version "1.7.22"
 	kotlin("plugin.jpa") version "1.7.22"
+	id("io.gitlab.arturbosch.detekt").version("1.22.0")
 }
 
 group = "com.ibartuszek"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
+// versions
+val kotlinLoggingVersion = "3.0.5"
+
 repositories {
 	mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	// Web
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	// JPA
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	runtimeOnly("org.postgresql:postgresql")
+
+	// Other
+	implementation("io.github.microutils:kotlin-logging-jvm:$kotlinLoggingVersion")
+
+	// Test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+detekt {
+	config = files("config/detekt/detekt.yml")
+	baseline = file("config/detekt/detekt-baseline.xml")
 }
 
 tasks.withType<KotlinCompile> {
